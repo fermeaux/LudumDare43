848e870854388e7a263ece4a46f797738653523b
Fermeaux <fermeaux.t@gmail.com>
2018-12-01 16:21:01 +0100

    Possibility to pick a value from game settings

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -5,6 +5,33 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameSettings", menuName = "GameSettings")]
 public class GameSettings : ScriptableObject
 {
-    public Question[] questions;
-    public Personality[] personalities;
+    public List<Question> questions;
+    public List<Personality> personalities;
+
+    private List<Question> seenQuestions;
+    private List<Personality> seenPersonalities;
+
+    public Question PickQuestion()
+    {
+        int index = Random.Range(0, questions.Count);
+        Question tmp = questions[index];
+        questions.RemoveAt(index);
+        seenQuestions.Add(tmp);
+        return tmp;
+    }
+
+    public Personality PickPersonality()
+    {
+        int index = Random.Range(0, personalities.Count);
+        Personality tmp = personalities[index];
+        personalities.RemoveAt(index);
+        seenPersonalities.Add(tmp);
+        return tmp;
+    }
+
+    public void Reset()
+    {
+        questions.AddRange(seenQuestions);
+        personalities.AddRange(seenPersonalities);
+    }
 }
